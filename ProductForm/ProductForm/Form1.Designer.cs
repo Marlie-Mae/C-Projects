7782d1a40ffaca0824409a4139154628761ecfce
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-10-30 18:30:18 +0800

    Add files via upload

---
+++ ProductForm/ProductForm/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace ProductForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtpid = new TextBox();
+            txtpname = new TextBox();
+            txtpdesc = new TextBox();
+            btnadd = new Button();
+            btnupdate = new Button();
+            btndelete = new Button();
+            btclear = new Button();
+            comboHistory = new ComboBox();
+            btnprev = new Button();
+            btnnext = new Button();
+            btnexit = new Button();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(252, 205, 42);
+            label1.Font = new Font("Bookman Old Style", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(52, 121, 40);
+            label1.Location = new Point(243, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(292, 75);
+            label1.TabIndex = 0;
+            label1.Text = "Product";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            label2.ForeColor = Color.FromArgb(52, 121, 40);
+            label2.Location = new Point(125, 186);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 18);
+            label2.TabIndex = 1;
+            label2.Text = "Product ID:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            label3.ForeColor = Color.FromArgb(52, 121, 40);
+            label3.Location = new Point(95, 230);
+            label3.Name = "label3";
+            label3.Size = new Size(139, 18);
+            label3.TabIndex = 2;
+            label3.Text = "Product Name:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            label4.ForeColor = Color.FromArgb(52, 121, 40);
+            label4.Location = new Point(48, 275);
+            label4.Name = "label4";
+            label4.Size = new Size(186, 18);
+            label4.TabIndex = 3;
+            label4.Text = "Product Description:";
+            // 
+            // txtpid
+            // 
+            txtpid.BackColor = Color.FromArgb(255, 251, 230);
+            txtpid.Font = new Font("Verdana", 12F);
+            txtpid.Location = new Point(247, 181);
+            txtpid.Name = "txtpid";
+            txtpid.Size = new Size(262, 27);
+            txtpid.TabIndex = 4;
+            // 
+            // txtpname
+            // 
+            txtpname.BackColor = Color.FromArgb(255, 251, 230);
+            txtpname.Font = new Font("Verdana", 12F);
+            txtpname.Location = new Point(247, 225);
+            txtpname.Name = "txtpname";
+            txtpname.Size = new Size(262, 27);
+            txtpname.TabIndex = 5;
+            // 
+            // txtpdesc
+            // 
+            txtpdesc.BackColor = Color.FromArgb(255, 251, 230);
+            txtpdesc.Font = new Font("Verdana", 12F);
+            txtpdesc.Location = new Point(247, 270);
+            txtpdesc.Name = "txtpdesc";
+            txtpdesc.Size = new Size(262, 27);
+            txtpdesc.TabIndex = 6;
+            // 
+            // btnadd
+            // 
+            btnadd.BackColor = Color.FromArgb(52, 121, 40);
+            btnadd.Font = new Font("Verdana", 9.75F, FontStyle.Bold);
+            btnadd.ForeColor = Color.FromArgb(244, 246, 255);
+            btnadd.Location = new Point(595, 152);
+            btnadd.Name = "btnadd";
+            btnadd.Size = new Size(90, 36);
+            btnadd.TabIndex = 7;
+            btnadd.Text = "Add";
+            btnadd.UseVisualStyleBackColor = false;
+            btnadd.Click += btnadd_Click;
+            // 
+            // btnupdate
+            // 
+            btnupdate.BackColor = Color.FromArgb(52, 121, 40);
+            btnupdate.Font = new Font("Verdana", 9.75F, FontStyle.Bold);
+            btnupdate.ForeColor = Color.FromArgb(244, 246, 255);
+            btnupdate.Location = new Point(595, 194);
+            btnupdate.Name = "btnupdate";
+            btnupdate.Size = new Size(90, 36);
+            btnupdate.TabIndex = 8;
+            btnupdate.Text = "Update";
+            btnupdate.UseVisualStyleBackColor = false;
+            btnupdate.Click += btnupdate_Click;
+            // 
+            // btndelete
+            // 
+            btndelete.BackColor = Color.FromArgb(52, 121, 40);
+            btndelete.Font = new Font("Verdana", 9.75F, FontStyle.Bold);
+            btndelete.ForeColor = Color.FromArgb(244, 246, 255);
+            btndelete.Location = new Point(595, 236);
+            btndelete.Name = "btndelete";
+            btndelete.Size = new Size(90, 36);
+            btndelete.TabIndex = 9;
+            btndelete.Text = "Delete";
+            btndelete.UseVisualStyleBackColor = false;
+            btndelete.Click += btndelete_Click;
+            // 
+            // btclear
+            // 
+            btclear.BackColor = Color.FromArgb(52, 121, 40);
+            btclear.Font = new Font("Verdana", 9.75F, FontStyle.Bold);
+            btclear.ForeColor = Color.FromArgb(244, 246, 255);
+            btclear.Location = new Point(595, 278);
+            btclear.Name = "btclear";
+            btclear.Size = new Size(90, 36);
+            btclear.TabIndex = 10;
+            btclear.Text = "Clear";
+            btclear.UseVisualStyleBackColor = false;
+            btclear.Click += btnclear_Click;
+            // 
+            // comboHistory
+            // 
+            comboHistory.BackColor = Color.FromArgb(255, 251, 230);
+            comboHistory.Font = new Font("Verdana", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            comboHistory.ForeColor = Color.FromArgb(52, 121, 40);
+            comboHistory.FormattingEnabled = true;
+            comboHistory.ItemHeight = 14;
+            comboHistory.Items.AddRange(new object[] { "Available Products", "Stock IN", "Stock OUT", "Product History" });
+            comboHistory.Location = new Point(606, 25);
+            comboHistory.Name = "comboHistory";
+            comboHistory.Size = new Size(148, 22);
+            comboHistory.TabIndex = 11;
+            comboHistory.SelectedIndexChanged += comboHistory_SelectedIndexChanged;
+            // 
+            // btnprev
+            // 
+            btnprev.BackColor = Color.FromArgb(52, 121, 40);
+            btnprev.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            btnprev.ForeColor = Color.FromArgb(244, 246, 255);
+            btnprev.Location = new Point(227, 357);
+            btnprev.Name = "btnprev";
+            btnprev.Size = new Size(95, 44);
+            btnprev.TabIndex = 12;
+            btnprev.Text = "Previous";
+            btnprev.UseVisualStyleBackColor = false;
+            btnprev.Click += btnprev_Click;
+            // 
+            // btnnext
+            // 
+            btnnext.BackColor = Color.FromArgb(52, 121, 40);
+            btnnext.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            btnnext.ForeColor = Color.FromArgb(244, 246, 255);
+            btnnext.Location = new Point(423, 357);
+            btnnext.Name = "btnnext";
+            btnnext.Size = new Size(95, 44);
+            btnnext.TabIndex = 13;
+            btnnext.Text = "Next";
+            btnnext.UseVisualStyleBackColor = false;
+            btnnext.Click += btnnext_Click;
+            // 
+            // btnexit
+            // 
+            btnexit.BackColor = Color.FromArgb(52, 121, 40);
+            btnexit.Font = new Font("Verdana", 9.75F, FontStyle.Bold);
+            btnexit.ForeColor = Color.FromArgb(244, 246, 255);
+            btnexit.Location = new Point(595, 320);
+            btnexit.Name = "btnexit";
+            btnexit.Size = new Size(90, 36);
+            btnexit.TabIndex = 14;
+            btnexit.Text = "Exit";
+            btnexit.UseVisualStyleBackColor = false;
+            btnexit.Click += btnexit_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(252, 205, 42);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(-12, -4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(791, 120);
+            panel1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.FromArgb(255, 251, 230);
+            ClientSize = new Size(775, 435);
+            Controls.Add(btnexit);
+            Controls.Add(btnnext);
+            Controls.Add(btnprev);
+            Controls.Add(comboHistory);
+            Controls.Add(btclear);
+            Controls.Add(btndelete);
+            Controls.Add(btnupdate);
+            Controls.Add(btnadd);
+            Controls.Add(txtpdesc);
+            Controls.Add(txtpname);
+            Controls.Add(txtpid);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtpid;
+        private TextBox txtpname;
+        private TextBox txtpdesc;
+        private Button btnadd;
+        private Button btnupdate;
+        private Button btndelete;
+        private Button btclear;
+        private ComboBox comboHistory;
+        private Button btnprev;
+        private Button btnnext;
+        private Button btnexit;
+        private Panel panel1;
+    }
+}
---
+++ ProductForm/ProductForm/Form5.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ProductForm
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboHistory = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtsearch = new TextBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // comboHistory
+            // 
+            comboHistory.FormattingEnabled = true;
+            comboHistory.Items.AddRange(new object[] { "Product", "Available Products", "Stock IN", "Stock OUT" });
+            comboHistory.Location = new Point(550, 27);
+            comboHistory.Name = "comboHistory";
+            comboHistory.Size = new Size(148, 23);
+            comboHistory.TabIndex = 14;
+            comboHistory.SelectedIndexChanged += comboHistory_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(200, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(325, 42);
+            label1.TabIndex = 13;
+            label1.Text = "Product History";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(71, 126);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 15;
+            label2.Text = "Search";
+            // 
+            // txtsearch
+            // 
+            txtsearch.Location = new Point(119, 118);
+            txtsearch.Name = "txtsearch";
+            txtsearch.Size = new Size(203, 23);
+            txtsearch.TabIndex = 16;
+            txtsearch.TextChanged += txtsearch_TextChanged;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(57, 151);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(618, 238);
+            dataGridView1.TabIndex = 17;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Form5
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(724, 408);
+            Controls.Add(dataGridView1);
+            Controls.Add(txtsearch);
+            Controls.Add(label2);
+            Controls.Add(comboHistory);
+            Controls.Add(label1);
+            Name = "Form5";
+            Text = "Form5";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboHistory;
+        private Label label1;
+        private Label label2;
+        private TextBox txtsearch;
+        private DataGridView dataGridView1;
+    }
+}
